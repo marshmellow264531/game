854420d221572a4dbd019237c21ec84aaf595bbb
marshmellow264531 <136032648+marshmellow264531@users.noreply.github.com>
2023-08-14 14:46:16 +1200

    yep

---
+++ game.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById("gameCanvas")
+const ctx = canvas.getContext("2d")
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 10,
+    dx: 2,
+    dy: -2
+};
+const paddle = {
+    width: 75,
+    height: 50,
+    x: (canvas.width - 75) / 2, 
+    y: canvas.height - 10
+};
+let rightPressed = false;
+let leftPressed = false;
