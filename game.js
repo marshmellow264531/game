8ae1afdf505669b7bf09b910e62effc59dc8e100
marshmellow264531 <136032648+marshmellow264531@users.noreply.github.com>
2023-08-18 12:25:51 +1200

    yip

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
 const canvas  =  document.getElementById('gameCanvas')
 const ctx = canvas.getContext('2D')
-
+const images = ["47f1b200-8b2e-11ea-8fa1-ab106189aeb0.jfif","47f1b200-8b2e-11ea-8fa1-ab106189aeb0.jfif",
+"photo-1481349518771-20055b2a7b24.jfif","photo-1481349518771-20055b2a7b24.jfif"];
+var shuf_images = images.sort(() => (Math.random() > .5) ? 2 : -1);
+for ( var i =0; i<images.length; i++){
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_images[i]
+    document.querySelector('.game').appendChild(box);
+}
