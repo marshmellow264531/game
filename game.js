5c964ca047e271edbe0bf738ee35fa2a2a974df2
marshmellow264531 <136032648+marshmellow264531@users.noreply.github.com>
2023-08-23 09:24:05 +1200

    y

--- game.js
+++ game.js
@@ -1,11 +1,58 @@
 const canvas  =  document.getElementById('gameCanvas')
 const ctx = canvas.getContext('2D')
-const images = ["47f1b200-8b2e-11ea-8fa1-ab106189aeb0.jfif","47f1b200-8b2e-11ea-8fa1-ab106189aeb0.jfif",
-"photo-1481349518771-20055b2a7b24.jfif","photo-1481349518771-20055b2a7b24.jfif"];
-var shuf_images = images.sort(() => (Math.random() > .5) ? 2 : -1);
-for ( var i =0; i<images.length; i++){
-    let box = document.createElement('div')
-    box.className = 'item';
-    box.innerHTML = shuf_images[i]
-    document.querySelector('.game').appendChild(box);
+document.addEventListener('click', event => {
+    const target = event.target
+    const isCell = target.classList.contains('grid-cell')
+    const isDisabled = target.classList.contains('disabled')
+
+    if (isCell && !isDisabled) {
+        const cellValue = target.dataset.value
+
+        game.xTurn === true
+            ? game.xState.push(cellValue)
+            : game.oState.push(cellValue)
+        
+        target.classList.add('disabled')
+        target.classList.add(game.xTurn ? 'x' : 'o')
+        
+        game.xTurn = !game.xTurn
+    }
+})
+if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
+    document.querySelector('.game-over').classList.add('visible')
+    document.querySelector('.game-over-text').textContent = 'Draw!'
 }
+game.winningStates.forEach(winningState => {
+    const xWins = winningState.every(state => game.xState.includes(state))
+    const oWins = winningState.every(state => game.oState.includes(state))
+  
+    if (xWins || oWins) {
+        document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
+        document.querySelector('.game-over').classList.add('visible')
+        document.querySelector('.game-over-text').textContent = xWins
+            ? 'X wins!'
+            : 'O wins!'
+    }
+})
+game.winningStates.forEach(winningState => {
+    const xWins = winningState.every(state => game.xState.includes(state))
+    const oWins = winningState.every(state => game.oState.includes(state))
+  
+    if (xWins || oWins) {
+        document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
+        document.querySelector('.game-over').classList.add('visible')
+        document.querySelector('.game-over-text').textContent = xWins
+            ? 'X wins!'
+            : 'O wins!'
+    }
+})
+document.querySelector('.restart').addEventListener('click', () => {
+    document.querySelector('.game-over').classList.remove('visible')
+    document.querySelectorAll('.grid-cell').forEach(cell => {
+        cell.classList.remove('disabled', 'x', 'o')
+    })
+
+    game.xTurn = true
+    game.xState = []
+    game.oState = []
+})
