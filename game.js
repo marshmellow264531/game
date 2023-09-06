fae6279ef1b1bf7f98f9e1fcdc7733de24256070
marshmellow264531 <136032648+marshmellow264531@users.noreply.github.com>
2023-09-07 10:17:55 +1200

    yip

--- game.js
+++ game.js
@@ -1,77 +1,285 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2D');
 
-const game = {
-    xTurn: true,
-    xState: [],
-    oState: [],
-    winningStates: [
-        // Rows
-        ['0', '1', '2'],
-        ['3', '4', '5'],
-        ['6', '7', '8'],
+const ball = { 
 
-        // Columns
-        ['0', '3', '6'],
-        ['1', '4', '7'],
-        ['2', '5', '8'],
+    x: canvas.width / 2, 
 
-        // Diagonal
-        ['0', '4', '8'],
-        ['2', '4', '6']
-    ]
-}
-document.addEventListener('click', event => {
-    const target = event.target
-    const isCell = target.classList.contains('grid-cell')
-    const isDisabled = target.classList.contains('disabled')
+    y: canvas.height / 2, 
 
-    if (isCell && !isDisabled) {
-        const cellValue = target.dataset.value
+    radius: 10, 
 
-        game.xTurn === true
-            ? game.xState.push(cellValue)
-            : game.oState.push(cellValue)
-        
-        target.classList.add('disabled')
-        target.classList.add(game.xTurn ? 'x' : 'o')
-        
-        game.xTurn = !game.xTurn
-    }
-})
-if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
-    document.querySelector('.game-over').classList.add('visible')
-    document.querySelector('.game-over-text').textContent = 'Draw!'
-}
-game.winningStates.forEach(winningState => {
-    const xWins = winningState.every(state => game.xState.includes(state))
-    const oWins = winningState.every(state => game.oState.includes(state))
-  
-    if (xWins || oWins) {
-        document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
-        document.querySelector('.game-over').classList.add('visible')
-        document.querySelector('.game-over-text').textContent = xWins
-            ? 'X wins!'
-            : 'O wins!'
-    }
-})
-game.winningStates.forEach(winningState => {
-    const xWins = winningState.every(state => game.xState.includes(state))
-    const oWins = winningState.every(state => game.oState.includes(state))
-  
-    if (xWins || oWins) {
-        document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
-        document.querySelector('.game-over').classList.add('visible')
-        document.querySelector('.game-over-text').textContent = xWins
-            ? 'X wins!'
-            : 'O wins!'
-    }
-})
-document.querySelector('.restart').addEventListener('click', () => {
-    document.querySelector('.game-over').classList.remove('visible')
-    document.querySelectorAll('.grid-cell').forEach(cell => {
-        cell.classList.remove('disabled', 'x', 'o')
-    })
+    dx: 2, 
 
-    game.xTurn = true
-    game.xState = []
-    game.oState = []
-})
+    dy: -2 
+
+}; 
+
+
+
+const paddle = { 
+
+    width: 75, 
+
+    height: 10, 
+
+    x: (canvas.width - 75) / 2, 
+
+    y: canvas.height - 10 
+
+}; 
+
+
+
+let rightPressed = false; 
+
+let leftPressed = false; 
+function drawBall() { 
+
+    ctx.beginPath(); 
+
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2); 
+
+    ctx.fillStyle = "#0095DD";}
+    function drawBall() { 
+
+        ctx.beginPath(); 
+ 
+        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2); 
+ 
+        ctx.fillStyle = "#0095DD"; 
+ 
+        ctx.fill(); 
+ 
+        ctx.closePath(); 
+ 
+    };
+ 
+   
+ 
+    function drawPaddle() { 
+ 
+        ctx.beginPath(); 
+ 
+        ctx.rect(paddle.x, paddle.y, paddle.width, paddle.height); 
+ 
+        ctx.fillStyle = "#0095DD"; 
+ 
+        ctx.fill(); 
+ 
+        ctx.closePath(); 
+ 
+    };
+ 
+   
+    function draw() { 
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+ 
+        drawBall(); 
+ 
+   
+ 
+        if (rightPressed && paddle.x < canvas.width - paddle.width) { 
+ 
+            paddle.x += 7; 
+ 
+        } else if (leftPressed && paddle.x > 0) { 
+ 
+            paddle.x -= 7; 
+ 
+        } 
+ 
+   
+ 
+        drawPaddle(); 
+ 
+        // Update ball position here 
+ 
+        requestAnimationFrame(draw); 
+ 
+    };
+ 
+   
+ 
+   
+ 
+    
+    document.addEventListener("keydown", (e) => { 
+
+        if (e.key === "Right" || e.key === "ArrowRight") { 
+ 
+            rightPressed = true; 
+ 
+        } else if (e.key === "Left" || e.key === "ArrowLeft") { 
+ 
+            leftPressed = true; 
+ 
+        } 
+ 
+    }); 
+ 
+   
+ 
+    document.addEventListener("keyup", (e) => { 
+ 
+        if (e.key === "Right" || e.key === "ArrowRight") { 
+ 
+            rightPressed = false; 
+ 
+        } else if (e.key === "Left" || e.key === "ArrowLeft") { 
+ 
+            leftPressed = false; 
+ 
+        } 
+ 
+    });  
+    function draw() { 
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+ 
+   
+ 
+        ball.x += ball.dx; 
+ 
+        ball.y += ball.dy; 
+ 
+   
+ 
+        if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) { 
+ 
+            ball.dx = -ball.dx; 
+ 
+        } 
+ 
+   
+ 
+        if (ball.y - ball.radius < 0) { 
+ 
+            ball.dy = -ball.dy; 
+ 
+        } else if (ball.y + ball.radius > canvas.height) { 
+ 
+            // Reset ball position if it hits the bottom 
+ 
+            ball.x = canvas.width / 2; 
+ 
+            ball.y = canvas.height / 2; 
+ 
+            ball.dx = 2; 
+ 
+            ball.dy = -2; 
+ 
+        } 
+ 
+   
+ 
+        drawBall(); 
+ 
+   
+ 
+        if (rightPressed && paddle.x < canvas.width - paddle.width) { 
+ 
+            paddle.x += 7; 
+ 
+        } else if (leftPressed && paddle.x > 0) { 
+ 
+            paddle.x -= 7; 
+ 
+        } 
+ 
+   
+ 
+        drawPaddle(); 
+ 
+        requestAnimationFrame(draw); 
+ 
+    };
+ 
+   
+ 
+   
+    function draw() { 
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+ 
+   
+ 
+        ball.x += ball.dx; 
+ 
+        ball.y += ball.dy; 
+ 
+   
+ 
+        // Ball collision with walls 
+ 
+        if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) { 
+ 
+            ball.dx = -ball.dx; 
+ 
+        } 
+ 
+   
+ 
+        // Ball collision with paddle 
+ 
+        if ( 
+ 
+            ball.y + ball.dy > canvas.height - ball.radius - paddle.height &&
+ 
+            ball.x > paddle.x &&
+ 
+            ball.x < paddle.x + paddle.width
+ 
+        ) 
+        { 
+ 
+            ball.dy = -ball.dy; 
+ 
+        } 
+ 
+   
+ 
+        // Ball collision with top 
+ 
+        if (ball.y - ball.radius < 0) { 
+ 
+            ball.dy = -ball.dy; 
+ 
+        } else if (ball.y + ball.radius > canvas.height) { 
+ 
+            ball.x = canvas.width / 2; 
+ 
+            ball.y = canvas.height / 2; 
+ 
+            ball.dx = 2; 
+ 
+            ball.dy = -2; 
+ 
+        } 
+ 
+   
+ 
+        drawBall(); 
+ 
+   
+ 
+        if (rightPressed && paddle.x < canvas.width - paddle.width) { 
+ 
+            paddle.x += 7; 
+ 
+        } else if (leftPressed && paddle.x > 0) { 
+ 
+            paddle.x -= 7; 
+ 
+        } 
+ 
+   
+ 
+        drawPaddle(); 
+ 
+        requestAnimationFrame(draw); 
+ 
+    };
+
+
